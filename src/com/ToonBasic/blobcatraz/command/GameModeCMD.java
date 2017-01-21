92794ba880988a33620481196f3863b3c2f6d2a6
SirBlobman <blobmansrealm@gmail.com>
2017-01-20 21:35:31 -0500

    Update 5
    Fixed GameModeCMD code since it was messy

--- src/com/ToonBasic/blobcatraz/command/GameModeCMD.java
+++ src/com/ToonBasic/blobcatraz/command/GameModeCMD.java
@@ -7,39 +7,71 @@ import org.bukkit.entity.Player;
 
 public abstract class GameModeCMD extends Manager {
 
-	String prefix = "�3{�bBlobcatraz�3} �f";
+	String prefix = "§3{§bBlobcatraz§3} §f";
+	public GameModeCMD() {super("gm", "blobcatraz.gamemode", false);}
 
-	public GameModeCMD() {
-		super("gm", "blobcatraz.gamemode", false);
-	}
-	public final void execute(final CommandSender sender, final String[] args) {
-		Player p = (Player) sender;
-		p.sendMessage(prefix + "Missing arguments! Did you mean /gm [0/1]?");
+	@Override
+	public final void execute(CommandSender cs, String[] args)
+	{
+	    super.execute(cs, args);
+	    if(cs instanceof Player)
+        {
+            Player p = (Player) cs;
+            if(args.length >= 1)
+            {
+                Player target = p;
+                GameMode gamemode = GameMode.SURVIVAL;
+                if(args.length > 1)
+                {
+                    String name = args[1];
+                    target = Bukkit.getPlayer(name);
+                    if(target == null)
+                    {
+                        p.sendMessage(prefix + "Invalid Target: " + name);
+                        return;
+                    }
+                }
 
-		if(args.length == 0) {
-			if(args[0].equalsIgnoreCase("1")) {
-				p.setGameMode(GameMode.CREATIVE);
-				p.sendMessage(prefix + "Your GameMode has been updated to CREATIVE!");
-			}
-			if(args[0].equalsIgnoreCase("0")) {
-				p.setGameMode(GameMode.SURVIVAL);
-				p.sendMessage(prefix + "Your GameMode has been updated to SURVIVAL!");
-			}
-			if(args.length == 1) {
-				Player target = Bukkit.getServer().getPlayer(args[0]);
-				if(target == null) {
-					p.sendMessage(prefix + "That player isn't online!");
-				}
-				if(args[0].equals("1")) {
-					target.setGameMode(GameMode.CREATIVE);
-					target.sendMessage(prefix + "Your GameMode has been updated to CREATIVE!");
-					p.sendMessage(prefix + "You have updated your target's GameMode to CREATIVE!");
-				} else
-					if(args[0].equals("0")) {
-						target.setGameMode(GameMode.SURVIVAL);
-						p.sendMessage(prefix + "You have updated your target's GameMode to SURVIVAL!");
-					}
-			}
-		}
+                String gm = args[0].toUpperCase();
+                gamemode = match(gm);
+                target.setGameMode(gamemode);
+                target.sendMessage(prefix + "Your GameMode was changed to " + gamemode.name());
+                p.sendMessage(prefix + "Success!");
+            }
+            else
+            {
+                p.sendMessage(prefix + "Missing Arguments!");
+                p.sendMessage("Usage: /gm [a/c/s/sp]");
+            }
+        }
+        else
+        {
+            cs.sendMessage("You are not a Player");
+            return;
+        }
 	}
+
+	private GameMode match(String gm)
+    {
+        switch(gm)
+        {
+            case "0":
+            case "S":
+            case "SURVIVAL":
+                return GameMode.SURVIVAL;
+            case "1":
+            case "C":
+            case "CREATIVE":
+                return GameMode.CREATIVE;
+            case "2":
+            case "A":
+            case "ADVENTURE":
+                return GameMode.ADVENTURE;
+            case "3":
+            case "SP":
+            case "SPECTATOR":
+                return GameMode.SPECTATOR;
+            default: return GameMode.SURVIVAL;
+        }
+    }
 }
