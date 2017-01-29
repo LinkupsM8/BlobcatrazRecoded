b95ac598d6e43c837d67783442a945d639771ddc
iDev <zyphemc@gmail.com>
2017-01-29 13:46:27 -0600

    Quick fix
    Due to the merge request, a bit of the progress in "Core.java" was lost
    due to the person who submitted the merge utilizing an outdated version
    of the "Core.java" which has now been fixed.

--- src/com/toonbasic/blobcatraz/Core.java
+++ src/com/toonbasic/blobcatraz/Core.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 import com.ToonBasic.blobcatraz.command.staff.*;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +13,7 @@ import com.ToonBasic.blobcatraz.command.CommandFramework;
 import com.ToonBasic.blobcatraz.command.player.CommandAFK;
 import com.ToonBasic.blobcatraz.command.player.CommandBalance;
 import com.ToonBasic.blobcatraz.command.player.CommandNickname;
+import com.ToonBasic.blobcatraz.command.player.warp.Warp;
 
 public class Core extends JavaPlugin {
     public static Core instance;
@@ -26,6 +28,7 @@ public class Core extends JavaPlugin {
         folder = getDataFolder();
         framework = new CommandFramework(instance);
         LOG.info("Now registering Blobcatraz...");
+        configs();
         commands();
         events();
     }
@@ -40,11 +43,11 @@ public class Core extends JavaPlugin {
         framework.registerCommand(new CommandFreeze());
         framework.registerCommand(new CommandGamemode());
         framework.registerCommand(new CommandItem());
-        framework.registerCommand(new CommandShowinv());
-        framework.registerCommand(new CommandSkull());
         framework.registerCommand(new CommandTempBan());
         framework.registerCommand(new CommandVanish());
         framework.registerCommand(new CommandMobSpawn());
+        framework.registerCommand(new CommandShowinv());
+        framework.registerCommand(new CommandSkull());
     //Player Commands
         framework.registerCommand(new CommandAFK());
         framework.registerCommand(new CommandBalance());
@@ -57,10 +60,14 @@ public class Core extends JavaPlugin {
         PluginManager pm = Bukkit.getPluginManager();
     //Staff Events
         pm.registerEvents(new CommandFreeze(), this);
-        pm.registerEvents(new CommandShowinv(), this);
         pm.registerEvents(new CommandVanish(), this);
+        pm.registerEvents(new CommandShowinv(), this);
     //Player Events
         pm.registerEvents(new CommandAFK(), this);
         pm.registerEvents(new CommandNickname(), this);
+    }
+    
+    public void configs() {
+    	ConfigurationSerialization.registerClass(Warp.class);
     }
 }
