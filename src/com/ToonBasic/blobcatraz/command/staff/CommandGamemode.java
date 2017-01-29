087920c678010d6d03e55fb5c29a9e8dbcf39a95
SirBlobman <blobmansrealm@gmail.com>
2017-01-28 22:47:16 -0500

    SirBlobman 15
    Added player argument to CommandGamemode

--- src/com/ToonBasic/blobcatraz/command/staff/CommandGamemode.java
+++ src/com/ToonBasic/blobcatraz/command/staff/CommandGamemode.java
@@ -1,25 +1,34 @@
 package com.ToonBasic.blobcatraz.command.staff;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.ToonBasic.blobcatraz.PublicHandlers;
 import com.ToonBasic.blobcatraz.command.ICommand;
 import com.ToonBasic.blobcatraz.command.ICommand.PlayerOnly;
 
 @PlayerOnly
 public class CommandGamemode extends ICommand {
-    public CommandGamemode() {super("gm", "<gamemode>", "blobcatraz.staff.gamemode", "gamemode");}
+    public CommandGamemode() {super("gm", "<gamemode> [player]", "blobcatraz.staff.gamemode", "gamemode");}
 
     @Override
     public void handleCommand(CommandSender cs, String[] args) {
         Player p = (Player) cs;
+        Player t = p;
         if(args.length > 0) {
+            if(args.length > 1) {
+            	t = Bukkit.getPlayer(args[0]);
+            	if(t == null) t = p;
+            }
+            
             GameMode gamemode = GameMode.SURVIVAL;
             String gm = args[0];
             gamemode = match(gm);
-            p.setGameMode(gamemode);
-            p.sendMessage(prefix + "Your GameMode was changed to " + gamemode.name());
+            t.setGameMode(gamemode);
+            t.sendMessage(prefix + PublicHandlers.color("Your GameMode was changed to &e" + gamemode.name()));
+            if(!t.getName().equals(p.getName())) p.sendMessage(prefix + PublicHandlers.color("&d" + t.getName() + " &f's GameMode was changed to &e" + gamemode.name()));
         } else {
             p.sendMessage("Missing Arguments: Did you mean /gm [gamemode]");
         }
