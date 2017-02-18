9c23413a0d2f23b41b330fc40751f0fe8600bdf0
SirBlobman <blobmansrealm@gmail.com>
2017-02-17 22:40:17 -0500

    SirBlobman 27
    Fixed /anvil
    Removed /furnace because furnaces need blocks/TileEntities to work

--- src/com/ToonBasic/blobcatraz/command/staff/CommandAnvil.java
+++ src/com/ToonBasic/blobcatraz/command/staff/CommandAnvil.java
@@ -1,28 +1,50 @@
 package com.ToonBasic.blobcatraz.command.staff;
 
-import com.ToonBasic.blobcatraz.command.ICommand;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
 
-/**
- * Plugin created by Lilith
- * MC-Market: http://www.mc-market.org/members/16269/
- * Spigot: https://www.spigotmc.org/members/razorkings.32987/
- * Redistribution is not permitted
- * If you find any errors, please send me a PM on either Spigot or MC-Market.
- */
+import com.ToonBasic.blobcatraz.command.ICommand;
+import com.ToonBasic.blobcatraz.command.ICommand.PlayerOnly;
+
+import net.minecraft.server.v1_11_R1.BlockPosition;
+import net.minecraft.server.v1_11_R1.ChatMessage;
+import net.minecraft.server.v1_11_R1.ContainerAnvil;
+import net.minecraft.server.v1_11_R1.EntityHuman;
+import net.minecraft.server.v1_11_R1.EntityPlayer;
+import net.minecraft.server.v1_11_R1.PacketPlayOutOpenWindow;
+import net.minecraft.server.v1_11_R1.PlayerConnection;
+
+@PlayerOnly
 public class CommandAnvil extends ICommand {
-    public CommandAnvil() {
-        super("anvil", "", "blobcatraz.staff.anvil");
-    }
+	
+	public CommandAnvil() {super("anvil", "", "blobcatraz.staff.anvil");}
+    
     @Override
     public void handleCommand(CommandSender cs, String[] args) {
-        Player player = (Player) cs;
-        Inventory anvil = Bukkit.createInventory(player, InventoryType.ANVIL);
-        player.openInventory(anvil);
-        player.sendMessage(prefix + "Now opening your anvil...");
+        Player p = (Player) cs;
+        if(p instanceof CraftPlayer) {
+        	CraftPlayer cp = (CraftPlayer) p;
+        	EntityPlayer ep = cp.getHandle();
+        	PlayerConnection pc = ep.playerConnection;
+        	FakeAnvil fa = new FakeAnvil(ep);
+        	int id = ep.nextContainerCounter();
+        	ChatMessage cm = new ChatMessage("Repairing");
+        	PacketPlayOutOpenWindow pow = new PacketPlayOutOpenWindow(id, "minecraft:anvil", cm, 0);
+        	pc.sendPacket(pow);
+        	
+        	ep.activeContainer = fa;
+        	ep.activeContainer.windowId = id;
+        	ep.activeContainer.addSlotListener(ep);
+        	ep.activeContainer = fa;
+        	ep.activeContainer.windowId = id;
+        }
+    }
+    
+    public final class FakeAnvil extends ContainerAnvil {
+    	public FakeAnvil(EntityPlayer ep) {super(ep.inventory, ep.world, new BlockPosition(0, 0, 0), ep);}
+    
+    	@Override
+    	public boolean a(EntityHuman eh) {return true;}
     }
 }
