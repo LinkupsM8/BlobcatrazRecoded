fb075ff0d297842e29f2c75ff42491d6f8772138
SirBlobman <blobmansrealm@gmail.com>
2017-02-19 12:50:09 -0500

    SirBlobman 31
    Added amount to /mob (max 1000)
    Fixed /lag not working in Console

--- src/com/ToonBasic/blobcatraz/command/staff/CommandMobSpawn.java
+++ src/com/ToonBasic/blobcatraz/command/staff/CommandMobSpawn.java
@@ -17,20 +17,26 @@ import com.ToonBasic.blobcatraz.utility.Util;
 @PlayerOnly
 public class CommandMobSpawn extends ICommand {
     public CommandMobSpawn() {
-        super("spawnmob", "<mob>", "blobcatraz.staff.spawnmob", "mob");
+        super("spawnmob", "<mob> [amount] ", "blobcatraz.staff.spawnmob", "mob");
     }
 
     @Override
     public void handleCommand(CommandSender cs, String[] args) {
         if (args.length > 0) {
+        	int amount = 1;
+        	if(args.length > 1) {
+        		try{amount = Integer.parseInt(args[1]);} 
+        		catch(Exception ex) {}
+        	}
         	Player p = (Player) cs;
         	Set<Material> air = null;
         	Block look = p.getTargetBlock(air, 100);
         	Location loc = look.getLocation();
         	loc.setY(loc.getY() + 1.5);
         	String mob = args[0].toUpperCase();
-        	if(spawn(loc, mob)) {
-        		String msg = Util.color(prefix + "&eEntity Spawned!");
+    		if(amount > 1000) amount = 1000;
+        	if(spawn(loc, mob, amount)) {
+        		String msg = Util.color(prefix + "&ESpawned &4" + amount+ " &eof " + mob);
         		p.sendMessage(msg);
         	} else {
         		String msg = Util.color(prefix + "&cInvalid Entity!");
@@ -39,12 +45,14 @@ public class CommandMobSpawn extends ICommand {
         }
     }
     
-    private boolean spawn(Location loc, String mob) {
+    private boolean spawn(Location loc, String mob, int amount) {
     	try {
     		EntityType et = EntityType.valueOf(mob);
         	if(et != null) {
         		World w = loc.getWorld();
-        		w.spawnEntity(loc, et);
+        		for(int i = amount; i > 0; i--) {
+            		w.spawnEntity(loc, et);
+        		}
         		return true;
         	} else return false;
     	} catch(Exception ex) {return false;}
