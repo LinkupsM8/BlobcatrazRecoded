c5f88cb52a95aaaa3f721d5bb83525cd96a5833f
SirBlobman <blobmansrealm@gmail.com>
2017-02-19 16:15:35 -0500

    SirBlobman 32
    Fixed CommandSpy having extra spaces and ignoring the database options

--- src/com/ToonBasic/blobcatraz/command/staff/CommandSpy.java
+++ src/com/ToonBasic/blobcatraz/command/staff/CommandSpy.java
@@ -10,31 +10,31 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import com.ToonBasic.blobcatraz.command.ICommand;
 import com.ToonBasic.blobcatraz.command.ICommand.PlayerOnly;
 import com.ToonBasic.blobcatraz.config.ConfigDatabase;
+import com.ToonBasic.blobcatraz.utility.Util;
 
 @PlayerOnly
 public class CommandSpy extends ICommand implements Listener {
 	
-	public CommandSpy() {
-		
-		super("spy", null, "blobcatraz.player.spy");
-		
-	}
+	public CommandSpy() {super("spy", "", "blobcatraz.staff.spy");}
 	
 	@Override
 	public void handleCommand(CommandSender sender, String[] args) {
-		
 		Player p = (Player) sender;
-		
 		ConfigDatabase.toggleSpy(p);
 		p.sendMessage("\u00a7aYou have toggled spy " + (ConfigDatabase.spy(p) ? "on" : "off") + ".");
 		
 	}
 	
 	@EventHandler
-	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e) {
-		
-		Player sender = e.getPlayer();
-		for (Player p : Bukkit.getServer().getOnlinePlayers()) if (!p.equals(sender)) p.sendMessage("\u00a7a<" + sender.getName() + "> " + e.getMessage());
+	public void cmd(PlayerCommandPreprocessEvent e) {	
+		Player send = e.getPlayer();
+		String cmd = e.getMessage();
+		for(Player p : Bukkit.getOnlinePlayers()) {
+			if(!p.equals(send) && ConfigDatabase.spy(p)) {
+				String msg = Util.color("&2<Spy: &a" + send.getName() + "&2> " + cmd);
+				p.sendMessage(msg);
+			}
+		}
 		
 	}
 }
