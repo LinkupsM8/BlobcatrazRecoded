98ca70806563393fd4aa09b9c998d88aed49fac1
SirBlobman <blobmansrealm@gmail.com>
2017-02-18 20:20:26 -0500

    SirBlobman 30
    Added some stuff to the To Do List
    removed extra spaces from CommandEmojis

--- src/com/ToonBasic/blobcatraz/command/player/CommandEmojis.java
+++ src/com/ToonBasic/blobcatraz/command/player/CommandEmojis.java
@@ -1,7 +1,5 @@
 package com.ToonBasic.blobcatraz.command.player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
@@ -12,50 +10,33 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.ToonBasic.blobcatraz.command.ICommand;
 import com.ToonBasic.blobcatraz.command.ICommand.PlayerOnly;
+import com.ToonBasic.blobcatraz.utility.Util;
 
 @PlayerOnly
 public class CommandEmojis extends ICommand implements Listener {
 	
-		private List<String> labelEmojis = new ArrayList<String>(Arrays.asList(":)", "<3", "^", "->", "<-"));
-		private List<String> inputEmojis = new ArrayList<String>(Arrays.asList(":\\)", "<3", "\\^", "->", "<-"));
-		private List<String> outputEmojis = new ArrayList<String>(Arrays.asList("\u263b", "\u2665", "\u2191", "\u2192", "\u2190"));
+		private List<String> labelEmojis = Util.newList(":)", "<3", "^", "->", "<-");
+		private List<String> inputEmojis = Util.newList(":\\)", "<3", "\\^", "->", "<-");
+		private List<String> outputEmojis = Util.newList("\u263b", "\u2665", "\u2191", "\u2192", "\u2190");
 
-		public CommandEmojis() {
-			
-			super("emojis", null, "blobcatraz.staff.emojis");
-			
-		}
+		public CommandEmojis() {super("emojis", null, "blobcatraz.staff.emojis");}
 
 		@Override
 		public void handleCommand(CommandSender sender, String[] args) {
-			
 			Player p = (Player) sender;
-			
 			StringBuilder msg = new StringBuilder("\u00a7lEmojis\u00a7r:\n");
-			
 			for (int i = 0; i < labelEmojis.size(); i++) {
-				
 				msg.append(labelEmojis.get(i) + " \u00a7lis\u00a7r " + outputEmojis.get(i) + "\n");
-				
 			}
-			
-			p.sendMessage(msg.toString()); 
-			
+			p.sendMessage(msg.toString());
 		}
 		
 		@EventHandler
 		public void onPlayerChat(AsyncPlayerChatEvent e) {
-			
 			String msg = e.getMessage();
-			
 			for (int i = 0; i < labelEmojis.size(); i++) {
-				
 				msg = msg.replaceAll(inputEmojis.get(i), outputEmojis.get(i));
-				
 			}
-			
 			e.setMessage(msg);
-			
 		}
-	
 }
