cbdfa3aa4311d1a5c70358e25d77cb349ebf7523
SirBlobman <blobmansrealm@gmail.com>
2017-03-31 12:33:35 -0400

    Fixed spelling/grammar mistakes in the rules

--- src/com/ToonBasic/blobcatraz/command/player/CommandRules.java
+++ src/com/ToonBasic/blobcatraz/command/player/CommandRules.java
@@ -12,10 +12,10 @@ public class CommandRules extends ICommand {
 		"&a1) &bDo not use cheats (hacks) or unapproved mods.\n&eOptifine is OK",
 		"&a2) &bDo not grief or build offensive things",
 		"&a3) &bHave common sense and be polite!",
-		"&eThe staff have the final say in wether you get banned or not. "
+		"&eThe staff have the final say in whether you get banned or not. "
 		+ "If you break any of these rules you can be "
 		+ "muted, kicked, temp-banned, or perma-banned, depending on the severity. "
-		+ "If you do get banned, you may appeal at &3&n&ohttp://blobcatraz.mc-srv.com\n"
+		+ "If you do get banned, you may appeal at &3&n&ohttp://blobcatraz.mc-srv.com\n&f"
 	});
 	public CommandRules() {super("rules", "", "blobcatraz.player.rules");}
 	
