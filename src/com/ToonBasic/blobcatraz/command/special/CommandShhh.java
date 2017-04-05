a7016f30cc5951f780af7c81822009bc14a26477
Jay <pingjaypeg@gmail.com>
2017-04-05 21:38:34 +0100

    JayBrmn 22
    Shhh... >:)

---
+++ src/com/ToonBasic/blobcatraz/command/special/CommandShhh.java
@@ -0,0 +1,13 @@
+package com.ToonBasic.blobcatraz.command.special;
+
+import org.bukkit.command.CommandSender;
+
+import com.ToonBasic.blobcatraz.command.ICommand;
+
+public class CommandShhh extends ICommand {
+	
+	public CommandShhh() { super("shhh"); }
+	
+	@Override
+	public void handleCommand(CommandSender cs, String[] args) { cs.sendMessage(prefix + "\u00a70Somewhere in the world, a Polar Bear has suddenly \u00a74\u00a7lDIED \u00a70! \u00a74\u00a7l>:)"); }
+}
