3734363e1e14c5e42951f13f75b140202ff8f982
SirBlobman <blobmansrealm@gmail.com>
2017-04-06 10:09:39 -0400

    SirBlobman 54
    Fixed the command that kills a random polar bear

--- src/com/ToonBasic/blobcatraz/command/special/CommandShhh.java
+++ src/com/ToonBasic/blobcatraz/command/special/CommandShhh.java
@@ -1,13 +1,29 @@
 package com.ToonBasic.blobcatraz.command.special;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.ToonBasic.blobcatraz.command.ICommand;
+import com.ToonBasic.blobcatraz.utility.Util;
 
 public class CommandShhh extends ICommand {
 	
 	public CommandShhh() { super("shhh", "", ""); }
 	
 	@Override
-	public void handleCommand(CommandSender cs, String[] args) { cs.sendMessage(prefix + "\u00a78Somewhere in the world, a Polar Bear has suddenly \u00a74\u00a7lDIED\u00a78! \u00a74\u00a7l>:)"); }
+	public void handleCommand(CommandSender cs, String[] args) {
+		String msg = prefix + Util.color("&7Somewhere in the world, "
+				+ "a &cToonBasic&7 has suddenly &4&lDIED! &7>:)");
+		cs.sendMessage(msg);
+		UUID toon = UUID.fromString("4e4bb25a-0245-42c0-9ef5-834c4622fa88");
+		OfflinePlayer op = Bukkit.getOfflinePlayer(toon);
+		if(op.isOnline()) {
+			Player p = op.getPlayer();
+			p.setHealth(0.0D);
+		}
+	}
 }
